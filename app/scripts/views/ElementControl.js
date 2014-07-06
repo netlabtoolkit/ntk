fa5b9968caa6c68ffde5e98b53c3908ab5d9f4b3
scazan <scottcazan@gmail.com>
2014-07-05 22:14:34 -0700

    cleaned out ElementControl's unnecessary code

--- app/scripts/views/ElementControl.js
+++ app/scripts/views/ElementControl.js
@@ -9,22 +9,6 @@ function(Backbone, WidgetView, Template){
 	return WidgetView.extend({
 		className: 'elementControl',
 		template: _.template(Template),
-		initialize: function(options) {
-			// Call the superclass constructor
-			WidgetView.prototype.initialize.call(this, options);
-
-			var self = this;
-
-			window.socketIO.on(this.model.get('inputMapping'), function(value) {
-				if(self.model) {
-					self.destinationModel.set(self.model.get('inputMapping'), value);
-				}
-				//self.$el.css(self.config.controlParameter, value * 3);
-				self.$('.element').css('left', (value/100) * window.innerWidth);
-			});
-
-		},
-
 	});
 });
 
