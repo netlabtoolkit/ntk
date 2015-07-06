fc89e18622bc2f9ee3fe9d41cb7e360b9807d6b4
phil <pva@philvanallen.com>
2015-07-06 12:42:24 -0700

    Widgets: change colors, renaming toolbar download -> export, load -> import

--- app/templates/ToolBar_tmpl.js
+++ app/templates/ToolBar_tmpl.js
@@ -1,8 +1,8 @@
 <div class="settings">
     <div class="logo"><img src="assets/images/ntk_logo.svg" alt="Netlab Toolkit"></div>
 	<div class="savePatch">Save</div>
-	<div class="downloadPatch">Download</div>
-	<div class="loadPatch">Load</div>
+	<div class="loadPatch">Import</div>
+    <div class="downloadPatch">Export</div>
 	<div class="clearPatch">Clear</div>
 	<form method="post" enctype="multipart/form-data" action="loadPatch" class="inputForm">
 		<input type="file" name="images" id="patchFileUpload" style="display:none" />
