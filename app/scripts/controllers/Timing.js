86323b35fc9055882a781ada0cb90a37dbc45cb6
Scott <scottcazan@gmail.com>
2015-07-22 00:35:40 -0700

    Fixing registerFrameCallback functions using proper context when checking for duplicates

--- app/scripts/controllers/Timing.js
+++ app/scripts/controllers/Timing.js
@@ -1,20 +1,18 @@
 define([
 ],
 function(){
-	var frameCount = 0,
-		self;
+	var frameCount = 0;
 
 	var TimingController = function() {
 		this.frameCallbacks = [];
 		this.originalCallbacks = [];
-		self = this;
 
-		window.requestAnimationFrame(this.tick);
+		window.requestAnimationFrame(this.tick.bind(this));
 	};
 
 	TimingController.prototype = {
 		tick: function(timeStamp) {
-			var callbacks = self.frameCallbacks;
+			var callbacks = this.frameCallbacks;
 
 			for(var i=callbacks.length-1; i>=0; i--) {
 				callbacks[i](frameCount, timeStamp);
@@ -22,22 +20,24 @@ function(){
 
 			frameCount++;
 
-			window.requestAnimationFrame(self.tick);
+			window.requestAnimationFrame( this.tick.bind(this) );
 		},
 		registerFrameCallback: function(callback, context) {
-			var indexIfExists = this.originalCallbacks.indexOf(callback);
+			var boundCallback = callback.bind(context);
+			var indexIfExists = this.originalCallbacks.indexOf(boundCallback);
 
 			if(indexIfExists >= 0) {
-				this.frameCallbacks[indexIfExists] = $.proxy( callback, context );
-				this.originalCallbacks[indexIfExists] = callback;
+				this.frameCallbacks[indexIfExists] = boundCallback;
+				this.originalCallbacks[indexIfExists] = boundCallback;
 			}
 			else {
-				this.frameCallbacks.push($.proxy( callback, context ));
-				this.originalCallbacks.push(callback);
+				this.frameCallbacks.push(boundCallback);
+				this.originalCallbacks.push(boundCallback);
 			}
 		},
 		removeFrameCallback: function(callback) {
-			var callbackIndex = this.originalCallbacks.indexOf(callback);
+			var boundCallback = callback.bind(context);
+			var callbackIndex = this.originalCallbacks.indexOf(boundCallback);
 
 			if(callbackIndex >= 0) {
 				this.frameCallbacks.splice(callbackIndex, 1);
