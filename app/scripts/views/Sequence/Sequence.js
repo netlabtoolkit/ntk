31204553630e8325305b2b43c94dec11daf46fd6
phil <pva@philvanallen.com>
2015-10-15 09:40:18 -0700

    Sequence: Made loop and return to values settable per individual sequence, fixed start/stop bug

--- app/scripts/views/Sequence/Sequence.js
+++ app/scripts/views/Sequence/Sequence.js
@@ -24,7 +24,10 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
 		],
 		outs: [
 			// title: decorative, from: <widget model field>, to: <widget model field being listened to>
-			{title: 'out1', from: 'output', to: 'out1'},
+			{title: 'out1', from: 'output0', to: 'out0'},
+            /*{title: 'out1', from: 'output1', to: 'out1'},
+            {title: 'out1', from: 'output2', to: 'out2'},
+            {title: 'out1', from: 'output3', to: 'out3'},*/
 		],
 		sources: [],
 		typeID: 'Sequence',
@@ -40,29 +43,29 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
             // Call any custom DOM events here
             this.model.set({
                 title: 'Sequence',
-                in0: 0,
-				in1: 0,
-                in2: 0,
-				in3: 0,
+                in0: 0, in1: 0, in2: 0, in3: 0,
+                out0: 0, out1: 0, out2: 0, out3: 0,
+                output0: 0, output1: 0, output2: 0, output3: 0,
                 lastIns: [-1,-1,-1,-1],
 				output: 0,
-                duration: 2000,
-                start: 0,
-                returnToStart: true,
-                loop: true,
+                loop0: true, loop1: true, loop2: true, loop3: true,
+                returnToStart0: true, returnToStart1: true, returnToStart2: true, returnToStart3: true,
+                start0: 0, start1: 0, start2: 0, start3: 0, 
+                duration0: 1000, duration1: 1000, duration2: 1000, duration3: 1000,
+                sendTo0: "output0", sendTo1: "output0", sendTo2: "output0", sendTo3: "output0", 
                 playSequence: false,
                 threshold: 512,
                 animationRunning: false,
                 lastInput: -1,
-                sequence0: "0,500,200\n500,100,500\n100,500,500\n1000,0,200",
-                sequence1: "0,500,400\n500,100,1000\n100,1000,500\n1000,0,400",
-                sequence2: "0,500,600\n500,100,1500\n100,1500,500\n1000,0,600",
-                sequence3: "0,500,800\n500,100,2000\n100,2000,500\n1000,0,800",
+                sequence0: "0,500,200\n500,100,500\n100,500,500\n500,0,200",
+                sequence1: "0,500,400\n500,100,1000\n100,500,1000\n500,0,400",
+                sequence2: "0,500,600\n500,100,1500\n100,500,1500\n500,0,600",
+                sequence3: "0,500,800\n500,100,2000\n100,500,2000\n500,0,800",
                 currentSequence: 0,
                 sequencePosition: 0,
+                domReady: false,
                 
             });
-
 
 		},
         /**
@@ -76,52 +79,72 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
             this.$(".animateDiv").css('visibility','hidden');
             this.$(".animateDiv").css('position','absolute');
             this.model.set('animationRunning',false); // in case it was true from last invocation
+            //if ((app.server && app.serverMode) || (!app.server && !app.serverMode)) {
+                this.$(".animateDiv").velocity("stop",true);
+            //}
             
-            if (this.model.get('loop') && this.model.get('in') == '--') {
-                this.runAnimation();
-            }
+            //console.log('starting ' + this.model.get('currentSequence'));
+            //console.log(this.model.get('out0'));
+            this.model.set('domReady',true);
+
+		},
+        
+        onRemove: function() {
+			this.$(".animateDiv").velocity("stop",true);
 		},
         
         onModelChange: function(model) {
-            if(model.changedAttributes().in0 !== undefined || 
-               model.changedAttributes().in1 !== undefined ||
-               model.changedAttributes().in2 !== undefined ||
-               model.changedAttributes().in3 !== undefined) {
-                
-                var ins = [];
-                ins[0] = parseFloat(this.model.get('in0'));
-                ins[1] = parseFloat(this.model.get('in1'));
-                ins[2] = parseFloat(this.model.get('in2'));
-                ins[3] = parseFloat(this.model.get('in3'));
-                
-                var threshold = parseFloat(this.model.get('threshold'));
-                
-                for (var i=0;i<4;i++) {
+            if (this.model.get('domReady') === true) {
+                if(model.changedAttributes().in0 !== undefined || 
+                   model.changedAttributes().in1 !== undefined ||
+                   model.changedAttributes().in2 !== undefined ||
+                   model.changedAttributes().in3 !== undefined) {
 
-                    if (ins[i] >= threshold && this.model.get('lastIns')[i] < threshold) {
-                        this.$(".animateDiv").velocity("stop");
-                        this.model.set('animationRunning',false);
-                        this.model.set('currentSequence',i);
-                        this.runAnimation();
+                    var ins = [];
+                    ins[0] = parseFloat(this.model.get('in0'));
+                    ins[1] = parseFloat(this.model.get('in1'));
+                    ins[2] = parseFloat(this.model.get('in2'));
+                    ins[3] = parseFloat(this.model.get('in3'));
 
-                    } else if (ins[i] < threshold  && this.model.get('lastIns')[i] >= threshold) {
-                        if (i === this.model.get('currentSequence')) {
-                            this.$(".animateDiv").velocity("stop");
+                    var threshold = parseInt(this.model.get('threshold'));
+                    //console.log(threshold + ' ' + ins[0] + ' ' + this.model.get('lastIns')[0]);
+
+                    for (var i=0;i<4;i++) {
+
+                        if (ins[i] >= threshold && this.model.get('lastIns')[i] < threshold) {
+                            //this.$(".animateDiv").velocity("stop",true);
+                            //console.log('starting');
                             this.model.set('animationRunning',false);
-                            if (this.model.get('returnToStart')) {
-                                this.returnAnimation();
+                            this.model.set('currentSequence',i);
+                            this.runAnimation();
+
+                        } else if (ins[i] < threshold  && this.model.get('lastIns')[i] >= threshold) {
+                            //console.log('cancel ' + this.model.get('currentSequence') + ' ' + i);
+                            if (i === this.model.get('currentSequence')) {
+                                //console.log('cancel really');
+                                this.$(".animateDiv").velocity("stop",true);
+                                this.model.set('animationRunning',false);
+                                var returnToStart = "returnToStart" + i.toString();
+                                if (this.model.get(returnToStart)) { 
+                                    this.returnAnimation();
+                                }
                             }
                         }
+                        this.model.get('lastIns')[i] = ins[i];
                     }
-                    this.model.get('lastIns')[i] = ins[i];
                 }
-            }
-            if(model.changedAttributes().loop !== undefined) {
-                if (this.model.get('loop') && this.model.get('in') == '--') {
-                    this.runAnimation();
-                } else if (!this.model.get('loop')) {
-                    this.$(".animateDiv").velocity("stop");
+/*                if(model.changedAttributes().loop0 !== undefined ) {
+                   if (!this.model.get('loop')) {
+                        this.$(".animateDiv").velocity("stop",true);
+                        this.model.set('animationRunning',false);
+                    }
+                }*/
+                if(model.changedAttributes().threshold !== undefined ) {
+                    this.$(".animateDiv").velocity("stop",true);
                     this.model.set('animationRunning',false);
+                    for (var i=0;i<4;i++) {
+                       this.model.get('lastIns')[i] = -1;
+                   }
                 }
             }
         },
@@ -132,17 +155,20 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
 
                 var self = this;
                 var animateDiv = this.$(".animateDiv");
-
+                animateDiv.velocity("stop",true);
                 if (!this.model.get('animationRunning')) {
 
                     // build a sequence from the text field in the widget
                     var userSequence = [];
                     var velocitySequence = [];
+                    var currentSequence = this.model.get('currentSequence').toString();
+                    
+                    var sequence = "sequence" + currentSequence;
+                    var loop = "loop" + currentSequence;
+                    var outputPort = this.model.get('sendTo' + currentSequence);
 
                     // get the string and parse it into an array
                     // the format for each move in the sequence is start, end, duration, one move to a line
-                    var sequence = "sequence" + this.model.get('currentSequence').toString();
-
                     var str = this.model.get(sequence);
                             
                     str = str.replace(/(\r\n|\n|\r)/gm, "\n");
@@ -161,14 +187,17 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
                               o: { 
                                   duration: item[2],
                                   progress: function(elements, c, r, s, t) {
-                                      self.model.set('output',t);
+                                      self.model.set(outputPort,t);
                                   },
                                   complete: function() {
                                       var currentPosition = 1 + self.model.get('sequencePosition');
                                       if (currentPosition >= sequenceLen) {
-                                          if (self.model.get('loop')) {
+                                          if (self.model.get(loop)) {
                                               self.model.set('sequencePosition',0);
-                                              $.Velocity.RunSequence(velocitySequence);
+                                              animateDiv.velocity("stop",true);
+                                              if (self.model.get('domReady') === true && self.model.get('animationRunning')) {
+                                                  $.Velocity.RunSequence(velocitySequence);
+                                              }
                                           } else {
                                               self.model.set('animationRunning',false);
                                           }
@@ -182,6 +211,7 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
                     } );
 
                     // execute the sequence
+                    this.model.set('animationRunning',true);
                     $.Velocity.RunSequence(velocitySequence);
                 }
             }
@@ -189,12 +219,18 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
         
         returnAnimation: function() {
             if ((app.server && app.serverMode) || (!app.server && !app.serverMode)) {
-                var duration = parseFloat(this.model.get('duration'));
-                var end = parseFloat(this.model.get('start'));
-                var start = this.model.get('output');
+                this.$(".animateDiv").velocity("stop",true);
+                
+                var currentSequence = this.model.get('currentSequence').toString();
+                
+                var duration = parseInt(this.model.get('duration' + currentSequence));
+                var end = parseFloat(this.model.get('start' + currentSequence));
+                var start = parseFloat(this.model.get('output0'));
 
                 var self = this;
                 var animateDiv = this.$(".animateDiv");
+                //var outputPort = this.model.get('sendTo' + currentSequence); 
+                var outputPort = 'output0';
 
                 animateDiv.velocity({
                     tween: [ end, start ]
@@ -202,7 +238,7 @@ function(Backbone, rivets, WidgetView, Template, SignalChainFunctions, SignalCha
                     duration: duration,
                     progress: function(elements, c, r, s, t) {
                         //console.log("The current tween value is " + t);
-                        self.model.set('output',t);
+                        self.model.set(outputPort,t);
                     },
                     complete: function() {
                         self.model.set('animationRunning',false);
--- app/scripts/views/Sequence/template.js
+++ app/scripts/views/Sequence/template.js
@@ -17,12 +17,13 @@
             <div class="inletValue"><span rv-text="widget:in1 | rounded">0</span> S2</div>
             <div class="inletValue"><span rv-text="widget:in2 | rounded">0</span> S3</div>
             <div class="inletValue"><span rv-text="widget:in3 | rounded">0</span> S4</div>
-                
-            
             
         </div>
         <div class="widgetBodyRight">  
-                <div class="inletValue"><span class="outputSingle" rv-text="widget:out1 | rounded">0</span></div>
+            <div class="inletValue"><span class="outputSingle" rv-text="widget:out0 | rounded">0</span></div>
+<!--            <div class="inletValue"><span class="outputSingle" rv-text="widget:out1 | rounded">0</span></div>
+            <div class="inletValue"><span class="outputSingle" rv-text="widget:out2 | rounded">0</span></div>
+            <div class="inletValue"><span class="outputSingle" rv-text="widget:out3 | rounded">0</span></div>-->
         </div>
     </div>
                 
@@ -38,20 +39,42 @@
         <div class="tab"><p>more</p></div>
         <div class="content">
             <label>threshold</label> <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:threshold"><br>
-            <input class="loop" type="checkbox" rv-checked="widget:loop" /> Loop<br>
-            <input class="return" type="checkbox" rv-checked="widget:returnToStart" /> Return to start value<br>
-            <label>start</label> <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:start"><br>
-            <label>duration</label> <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:duration"><br>
-
             <hr>
-            Sequence 1<br>
-            <textarea class="database" rv-value="widget:sequence0" rows="4" cols="70"></textarea><br>
-            Sequence 2<br>
-            <textarea class="database" rv-value="widget:sequence1" rows="4" cols="70"></textarea><br>
-            Sequence 3<br>
-            <textarea class="database" rv-value="widget:sequence2" rows="4" cols="70"></textarea><br>
-            Sequence 4<br>
-            <textarea class="database" rv-value="widget:sequence3" rows="4" cols="70"></textarea><br>
+            <strong>Sequence 1</strong>, &nbsp;&nbsp;<input class="loop" type="checkbox" rv-checked="widget:loop0" /> &nbsp;loop, &nbsp;&nbsp;<input class="return" type="checkbox" rv-checked="widget:returnToStart0" /> return to <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:start0"> with a duration of <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:duration0"><br>
+            <!--Send output to 
+            <input name="sendTo0" type="radio" value='output0' rv-checked="widget:sendTo0"> 1 
+            <input name="sendTo0" type="radio" value='output1' rv-checked="widget:sendTo0"> 2 
+            <input name="sendTo0" type="radio" value='output2' rv-checked="widget:sendTo0"> 3 
+            <input name="sendTo0" type="radio" value='output3' rv-checked="widget:sendTo0"> 4 -->
+            
+            <textarea class="database" rv-value="widget:sequence0" rows="4" cols="70"></textarea>
+            <br><br>
+            <strong>Sequence 2</strong>, &nbsp;&nbsp;<input class="loop" type="checkbox" rv-checked="widget:loop1" /> &nbsp;loop, &nbsp;&nbsp;<input class="return" type="checkbox" rv-checked="widget:returnToStart1" /> return to <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:start1"> with a duration of <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:duration1"><br>
+            <!--Send output to 
+            <input name="sendTo1" type="radio" value='output0' rv-checked="widget:sendTo1"> 1 
+            <input name="sendTo1" type="radio" value='output1' rv-checked="widget:sendTo1"> 2 
+            <input name="sendTo1" type="radio" value='output2' rv-checked="widget:sendTo1"> 3 
+            <input name="sendTo1" type="radio" value='output3' rv-checked="widget:sendTo1"> 4 -->
+            
+            <textarea class="database" rv-value="widget:sequence1" rows="4" cols="70"></textarea>
+            <br><br>
+            <strong>Sequence 3</strong>, &nbsp;&nbsp;<input class="loop" type="checkbox" rv-checked="widget:loop2" /> &nbsp;loop, &nbsp;&nbsp;<input class="return" type="checkbox" rv-checked="widget:returnToStart2" /> return to <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:start2"> with a duration of <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:duration2"><br>
+            <!--Send output to 
+            <input name="sendTo2" type="radio" value='output0' rv-checked="widget:sendTo2"> 1 
+            <input name="sendTo2" type="radio" value='output1' rv-checked="widget:sendTo2"> 2 
+            <input name="sendTo2" type="radio" value='output2' rv-checked="widget:sendTo2"> 3 
+            <input name="sendTo2" type="radio" value='output3' rv-checked="widget:sendTo2"> 4 -->
+           
+            <textarea class="database" rv-value="widget:sequence2" rows="4" cols="70"></textarea>
+            <br><br>
+            <strong>Sequence 4</strong>, &nbsp;&nbsp;<input class="loop" type="checkbox" rv-checked="widget:loop3" /> &nbsp;loop, &nbsp;&nbsp;<input class="return" type="checkbox" rv-checked="widget:returnToStart3" /> return to <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:start3"> with a duration of <input class="moreParam" type="text" pattern="[0-9]*" rv-value="widget:duration3"><br>
+            <!--Send output to 
+            <input name="sendTo3" type="radio" value='output0' rv-checked="widget:sendTo3"> 1 
+            <input name="sendTo3" type="radio" value='output1' rv-checked="widget:sendTo3"> 2 
+            <input name="sendTo3" type="radio" value='output2' rv-checked="widget:sendTo3"> 3 
+            <input name="sendTo3" type="radio" value='output3' rv-checked="widget:sendTo3"> 4 -->
+            
+            <textarea class="database" rv-value="widget:sequence3" rows="4" cols="70"></textarea>
         </div>
         <div class="animateDiv"></div>
     </div>
