b610fcd6a479239b728e1285752e398fc82c048f
Scott <scottcazan@gmail.com>
2015-11-27 09:40:40 -0800

    uncommenting hardware which was commented for testing

--- server/netlabServer.js
+++ server/netlabServer.js
@@ -12,7 +12,7 @@ var serverPort = 9001;
 
 // The currently selected/attached hardware devices
 var deviceControllers = {};
-//deviceControllers[deviceType] = new nlHardware({deviceType: deviceType}).model;
+deviceControllers[deviceType] = new nlHardware({deviceType: deviceType}).model;
 deviceControllers['OSC'] = new nlHardware({deviceType: 'OSC'}).model;
 
 // Create a WEB SERVER then create a transport tied to the webserver
