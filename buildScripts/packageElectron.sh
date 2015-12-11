6a6e02803dc31fab6bf0bcff157b9e6bc0652434
scazan <scottcazan@gmail.com>
2015-12-10 17:11:29 -0800

    Adding icons to package

--- buildScripts/packageElectron.sh
+++ buildScripts/packageElectron.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 mkdir ./packaged
 
-# LINUX
-./node_modules/.bin/electron-packager ./server NTK --platform=all --arch=x64 --version=0.35.1 --overwrite --out=packaged
+# ALL
+#./node_modules/.bin/electron-packager ./server NTK --platform=all --arch=x64 --version=0.35.1 --overwrite --out=packaged
 
 # OSX
-#./node_modules/.bin/electron-packager ./server NTK --platform=darwin --arch=x64 --version=0.35.1 --overwrite --out=packaged
+./node_modules/.bin/electron-packager ./server NTK --platform=darwin --arch=x64 --version=0.35.1 --overwrite --out=packaged --icon=server/icons/icon.icns
+
+# WIN
+#./node_modules/.bin/electron-packager ./server NTK --platform=win32 --arch=x64 --version=0.35.1 --overwrite --out=packaged --icon=server/icons/icon.ico
