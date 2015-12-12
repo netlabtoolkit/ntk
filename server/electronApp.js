0fa8783271e5b1de096d3bf3f701f62def9be38d
Scott <scottcazan@gmail.com>
2015-12-12 11:28:01 -0800

    Adding copy/paste

--- server/electronApp.js
+++ server/electronApp.js
@@ -1,5 +1,6 @@
 
 const electron = require('electron');
+var Menu = require("menu");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ntk = require('./netlabServer.js')();
@@ -32,5 +33,28 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
+
+  // Create the Application's main menu
+  var template = [{
+	  label: "Application",
+	  submenu: [
+		  { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+		  { type: "separator" },
+		  { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+	  ]}, {
+		  label: "Edit",
+		  submenu: [
+			  { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+			  { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+			  { type: "separator" },
+			  { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+			  { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+			  { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+			  { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+		  ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
