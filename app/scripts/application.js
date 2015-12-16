83acd8649113885c981c7ef96c1aeed9eac52cf1
Scott <scottcazan@gmail.com>
2015-12-15 18:57:05 -0800

    Adding update notice and bumping version

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -29,5 +29,42 @@ function( Backbone, Communicator, MainRouter, PatcherModule, ToolBarModule) {
 		App.module('ToolBar', ToolBarModule);
 	});
 
+	var currentVersion;
+
+	$.getJSON('package.json', function(localData) {
+		currentVersion = localData.version.split('.');
+
+		console.log('current', currentVersion);
+
+		$.getJSON('https://raw.githubusercontent.com/netlabtoolkit/ntk/master/package.json', function(data) {
+			var latestVersion = data.version.split('.');
+
+			var breakLoop = false,
+				versionIsCurrent = true,
+				i = 0;
+
+			while(i < 3 && !breakLoop) {
+				if(parseInt(latestVersion[i], 10) > parseInt(currentVersion[i],10)) {
+					breakLoop = true;
+					versionIsCurrent = false;
+				}
+				else if(parseInt(latestVersion[i], 10) !== parseInt(currentVersion[i],10) ) {
+					breakLoop = true;
+					versionIsCurrent = true;
+				}
+
+				i++;
+			}
+
+			console.log('versionIsCurrent', versionIsCurrent);
+			if(versionIsCurrent) {
+				$('.settings').append('<div class="version">v'+localData.version+'</div>');
+			}
+			else {
+				$('.settings').append('<div class="version"><a href="http://www.netlabtoolkit.org/download/" TARGET="_BLANK">UPDATE AVAILABLE</a></div>');
+			}
+		});
+	});
+
 	return App;
 });
