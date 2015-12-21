d2ab07b3ff3f4f57a10c087fd393eef8729d81ac
Scott <scottcazan@gmail.com>
2015-12-20 16:52:26 -0800

    Adding version to app menu

--- buildScripts/packageElectron.sh
+++ buildScripts/packageElectron.sh
@@ -5,7 +5,7 @@ mkdir ./packaged
 #./node_modules/.bin/electron-packager ./server NTK --platform=all --arch=x64 --version=0.35.1 --overwrite --out=packaged
 
 # OSX
-./node_modules/.bin/electron-packager ./server NTK --platform=darwin --arch=x64 --version=0.35.1 --overwrite --out=packaged --icon=server/icons/icon.icns
+./node_modules/.bin/electron-packager ./server NTK --platform=darwin --arch=x64 --version=0.35.1 --overwrite --out=packaged --icon=server/icons/icon.icns --app-version=1.0.0
 
 # WIN
 #./node_modules/.bin/electron-packager ./server NTK --platform=win32 --arch=x64 --version=0.35.1 --overwrite --out=packaged --icon=server/icons/icon.ico
