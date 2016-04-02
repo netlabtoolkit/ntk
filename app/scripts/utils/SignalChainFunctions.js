e615b2e0d9f3dc7f0fefa9d1a0e5c979a793cde4
Scott <scottcazan@gmail.com>
2016-04-01 21:50:02 -0700

    more contained approach to signal chains

--- app/scripts/utils/SignalChainFunctions.js
+++ app/scripts/utils/SignalChainFunctions.js
@@ -2,41 +2,42 @@ define([],
 function () {
 	// Static signal chain functions
 	return {
-        /**
-         * Scales a signal
+		/**
+		 * Scales a signal
 		 * Requires that the configuration options are present on the Widget's model
-         *
-         * @param {number} input
-         * @return {number}
-         */
-		scale: function(input) {
+		 *
+		 * @param {number} input
+		 * @return {number}
+		 */
+		scale: function(input, model) {
 			var output,
-			inputCeiling = parseInt(this.model.get('inputCeiling'),10),
-			outputCeiling = parseInt(this.model.get('outputCeiling'),10),
-			inputFloor = parseInt(this.model.get('inputFloor'),10),
-			outputFloor = parseInt(this.model.get('outputFloor'),10);
+				inputCeiling = parseInt(model.inputCeiling, 10),
+				outputCeiling = parseInt(model.outputCeiling, 10),
+				inputFloor = parseInt(model.inputFloor, 10),
+				outputFloor = parseInt(model.outputFloor, 10);
 
 			// process data here
 			var inputRange = inputCeiling - inputFloor,
 				outputRange = outputCeiling - outputFloor;
 
 			var scalingFactor = outputRange / inputRange;
-				output = ((parseFloat( input, 10) - inputFloor) * scalingFactor) + outputFloor;
-            
+
+			output = ((parseFloat( input, 10) - inputFloor) * scalingFactor) + outputFloor;
+
 			return output;
 		},
-        /**
-         * Inverts a signal
-         *
-         * @param {number} input
-         * @return {number}
-         */
-		invert: function(input) {
-			var output = parseFloat(input,10);
+		/**
+		 * Inverts a signal
+		 *
+		 * @param {number} input
+		 * @return {number}
+		 */
+		invert: function(input, model) {
+			var output = parseFloat(input, 10);
 
-			if(this.model.get('invert')) {
-                
-				output = (output - (output * 2)) + parseInt(this.model.get('outputCeiling'),10);
+			if(model.invert) {
+
+				output = (output - (output * 2)) + parseInt(model.outputCeiling, 10);
 			}
 
 			return output;
