a4bc4ac8185681ac983b4064c36d0f516290447e
Philip van Allen <dev@philvanallen.com>
2016-12-31 17:15:33 -0800

    added width input

--- app/scripts/views/Image/Image.js
+++ app/scripts/views/Image/Image.js
@@ -13,9 +13,10 @@ function(Backbone, rivets, WidgetView, Template){
 		className: 'image',
 		template: _.template(Template),
 		sources: [],
-        
+
         widgetEvents: {
 			'mouseup .detachedEl': 'imgMoved',
+      'change .displayWidth': 'setImageDimensions',
 		},
 		initialize: function(options) {
 			WidgetView.prototype.initialize.call(this, options);
@@ -26,7 +27,8 @@ function(Backbone, rivets, WidgetView, Template){
 					//{name: 'in', to: 'in'},
 					{title: 'X Position', to: 'left'},
 					{title: 'Y Position', to: 'top'},
-                    {title: 'opacity', to: 'opacity'},
+          {title: 'opacity', to: 'opacity'},
+          {title: 'width', to: 'displayWidth'},
 				],
 				title: 'Image',
 				activeControlParameter: 'left',
@@ -44,39 +46,50 @@ function(Backbone, rivets, WidgetView, Template){
 						parameter: 'opacity',
 					},
 				],
-				left: 100,
-                top: 200,
+        displayWidth: 500,
+        left: 100,
+        top: 200,
 				opacity: 100,
 			});
 		},
-        
+
         onRender: function() {
-			WidgetView.prototype.onRender.call(this);
-			var self = this;
-            if(!app.server) {
-                this.$( '.detachedEl' ).css( 'cursor', 'move' );
-                this.$( '.detachedEl' ).css( 'position', 'fixed' );
-                this.$( '.detachedEl' ).draggable({ cursor: 'move' });
+    		  WidgetView.prototype.onRender.call(this);
+    			var self = this;
+                if(!app.server) {
+                    this.$( '.detachedEl' ).css( 'cursor', 'move' );
+                    this.$( '.detachedEl' ).css( 'position', 'fixed' );
+                    this.$( '.detachedEl' ).draggable({ cursor: 'move' });
+                }
+    		},
+
+        onModelChange: function(model) {
+            if(model.changedAttributes().displayWidth !== undefined) {
+              this.setImageDimensions();
             }
-		},
-        
+          },
+
         imgMoved: function(e) {
             var offset = this.$('.detachedEl').offset();
             this.model.set('left',offset.left);
             this.model.set('top',offset.top);
             this.sendToFront();
         },
-        
+
         sendToFront: function() {
             var index_highest = 0;
             $(".widget").each(function () {
                 index_highest = Math.max(parseInt($(this).zIndex()), index_highest);
             });
-            
+
             this.$el.zIndex(index_highest + 1);
-        }
+        },
+
+        setImageDimensions: function() {
+            if(!app.server) {
+                this.$( '.detachedEl' ).css( 'width', this.model.get('displayWidth'));
+            }
+        },
 
 	});
 });
-
-
