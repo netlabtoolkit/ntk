0f348b1cb90a4a20fc78d08ad7abe5f4f78fde62
Scott <scottcazan@gmail.com>
2017-03-24 18:13:52 -0700

    Updating naming in GUI

--- app/scripts/views/AnalogIn/template.js
+++ app/scripts/views/AnalogIn/template.js
@@ -46,8 +46,8 @@
         <div class="content">
             input range<br>
             <label class="narrowLabel">Device</label> <select class="moreParam" type="text" rv-value="widget:deviceType">
-				<option selected value="ArduinoUno">Arduino Standard Firmata</option>
-				<option selected value="mkr1000">MKR1000</option>
+				<option selected value="ArduinoUno">Serial</option>
+				<option selected value="mkr1000">Network</option>
 			</select>
             <label class="narrowLabel">address</label> <input class="address" type="text" pattern="[0-9]*" rv-value="widget:server">
             <label class="narrowLabel">port</label> <input class="port" type="text" pattern="[0-9]*" rv-value="widget:port">
